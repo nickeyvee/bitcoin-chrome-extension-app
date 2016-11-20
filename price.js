98c850a77e5b6aa53fd97e00d1d1bec91c0e6fe5
Nicholas Vorraso <nvorraso2020@gmail.com>
2016-11-20 18:08:11 +0000

    init new js file

---
+++ price.js
@@ -0,0 +1,10 @@
+var marketData = "https://api.bitcoinaverage.com/ticker/global/USD";
+
+$.ajax({
+    "type": "GET",
+    "url": marketData,
+    "datatype": "JSON",
+    success : function(data){
+        
+    }
+})
