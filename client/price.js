1c4b7313882a57e88d585defdd173d41d4144a59
Nicholas Vorraso <nvorraso2020@gmail.com>
2016-11-22 03:55:56 +0000

    ether and css

--- client/price.js
+++ client/price.js
@@ -1,10 +1,52 @@
-var marketData = "https://api.bitcoinaverage.com/ticker/global/USD";
+$(".ethData").hide();
+
+var bitcoinData= "https://coinmarketcap-nexuist.rhcloud.com/api/btc",
+    ethereumData= "https://coinmarketcap-nexuist.rhcloud.com/api/eth";
 
 $.ajax({
     "type": "GET",
-    "url": marketData,
+    "url": bitcoinData,
     "datatype": "JSON",
     success : function(data){
-        $("#btc").html("$" + data.last);
-        }
-    })
+		var price = data.price.usd,
+			change = data.change;
+		price = price.toFixed(2);
+        $("#btcPrice").html("$ " + price);
+		$("#btcChange").html(change + "%");
+		
+		if(change < 0){
+   			$("#btcChange").css("color","red");
+   		} else {
+			$("#btcChange").css("color","green");
+		}
+    }
+});
+
+$.ajax({
+    "type": "GET",
+    "url": ethereumData,
+    "datatype": "JSON",
+    success: function(data2){
+        var price = data2.price.usd,
+			change = data2.change;
+        price = price.toFixed(2);
+        $("#ethPrice").html("$ " + price);
+		$("#ethChange").html(change + "%");
+		
+		if(change < 0){
+   			$("#ethChange").css("color","red");
+   		} else {
+			$("#ethChange").css("color","green");
+		}
+    }
+});
+
+$(".btc").click(function(){
+	$(".ethData").hide()
+	$(".btcData").show()
+});
+
+$(".eth").click(function(){
+	$(".btcData").hide()
+	$(".ethData").show()
+});
